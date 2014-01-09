49ae53ad4fba131c5840427c0ff09ecf31a7c747
Jeff Ward <jeff.ward@gmail.com>
2014-01-08 22:16:16 -0700

    compile.sh updated to use ascshd if it exists (see https://github.com/jcward/ascsh)

--- compile.sh
+++ compile.sh
@@ -1,2 +1,19 @@
+#!/bin/sh
+
 # swf-version=19 for AIR 3.6, see: http://stackoverflow.com/a/19099947/1026023
-$AIR_HOME/bin/mxmlc -source-path+=. +configname=air -swf-version=19 -debug -optimize=false -compress=false Main.as -output Main.swf
+ARGS='-source-path+=. +configname=air -swf-version=19 -debug -optimize=false -compress=false Main.as -output Main.swf'
+
+if [ -d $AIR_HOME ];
+then
+  # see: https://github.com/jcward/ascsh
+  if [ -x $AIR_HOME/bin/ascshd ];
+  then
+    echo "Using ascshd, if you experience weird errors, try \$AIR_HOME/bin/ascshd kill"
+    $AIR_HOME/bin/ascshd mxmlc $ARGS
+  else
+    echo "Using mxmlc..."
+    $AIR_HOME/bin/mxmlc $ARGS
+  fi
+else
+  echo "Requires \$AIR_HOME environment variable to point to AIR SDK location"
+fi
