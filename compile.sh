72b38556548e9b1669c275bab9d4010a925025bc
jeff.ward <jeff.ward@nimbee.com>
2014-01-08 21:46:20 -0700

    Added compile args to safely compile for AIR namespace 3.6 (avoids error loading initial content when compiling with newer compiler)

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-$AIR_HOME/bin/mxmlc -source-path+=. -debug -optimize=false -compress=false Main.as -output Main.swf
+# swf-version=19 for AIR 3.6, see: http://stackoverflow.com/a/19099947/1026023
+$AIR_HOME/bin/mxmlc -source-path+=. +configname=air -swf-version=19 -debug -optimize=false -compress=false Main.as -output Main.swf
